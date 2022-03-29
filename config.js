4482fba8de05a24ccb01f3c56a3381d0d900a1ae
YANZGANZ <102246383+YANZGANZ@users.noreply.github.com>
2022-03-29 21:56:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
-global.owner = [['6281227144597', 'ozan', true]] // Put your number here
+global.owner = [['48699532476', '𝒀𝑨𝑵𝒁', true]] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -25,9 +25,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Antum punya>>>...'
+global.packname = 'Dapp punya>>>...'
 global.author = ''
-global.wm = 'ZaBot-MD'
+global.wm = '𝒀𝑨𝑵𝒁Bot-MD'
 
 global.multiplier = 69 // The higher, The harder levelup
 
